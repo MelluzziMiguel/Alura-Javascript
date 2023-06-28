d3f6de42ca0008ae88164c9cfe0824c529b2cca7
Miguel Melluzzi <110363714+MelluzziMiguel@users.noreply.github.com>
2023-06-28 09:06:37 -0300

    Add files via upload

---
+++ 2-trabalhandocomvariaveis.js
@@ -0,0 +1,5 @@
+console.log("Trabalhando com variáveis");
+
+const idade = 16;
+const nome = "Miguel";
+let ano = 2023
---
+++ 5-atribuicaoVariaveis.js
@@ -0,0 +1,8 @@
+console.log("TRabalhando com atribuição de variáveis");
+const idade = 16
+const nome = "Miguel";
+const sobrenome = "Melluzzi";
+
+console.log(nome, sobrenome)
+
+
