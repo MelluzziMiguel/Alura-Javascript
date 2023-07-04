b1c87ddccb902f4876598f5fbcdd8d3c46b96b8e
Miguel Melluzzi <110363714+MelluzziMiguel@users.noreply.github.com>
2023-07-04 09:56:26 -0300

    Add files via upload

--- 5-atribuicaoVariaveis.js
+++ 5-atribuicaoVariaveis.js
@@ -1,8 +1,7 @@
-console.log("TRabalhando com atribuição de variáveis");
+console.log("Trabalhando com atribuição de variáveis");
 const idade = 16
 const nome = "Miguel";
 const sobrenome = "Melluzzi";
 
 console.log(nome, sobrenome)
-
-
+console.log('Meu nome é ${nome}');
